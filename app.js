184d2882117fef5ae69c32c92086b7d862d11ed6
clucle <wjdenwls123@gmail.com>
2018-06-25 18:09:01 +0900

    feat: basic websever node

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.all('*', function(req, res) {
+	res.render('index', {msg: 'Welcome to the Practical Node js'})
+})
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listeneing on port ' + app.get('port'));
+})
